440d880f5038f1b6f617b4d5fe0ea0733d223ded
Dzmitry Holubeu <dholubev1403@gmail.com>
2024-03-21 13:24:54 +0300

    Constants moved to util class

---
+++ src/main/java/com/dholubeu/driverservice/util/Constants.java
@@ -0,0 +1,8 @@
+package com.dholubeu.driverservice.util;
+
+public class Constants {
+    public static final String RESOURCE_ALREADY_EXISTS_MESSAGE =
+            "Car with number %s has been already registered";
+    public static final String RESOURCE_DOES_NOT_EXIST_BY_ID_MESSAGE = "Car with id %d does not exist";
+
+}
